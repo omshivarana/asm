21dc37acb29d8983e31c1e56973b26275721a7b9
Omshiva Rana <161568312+omshivarana@users.noreply.github.com>
2024-06-03 18:15:22 +0530

    Create crontabInstall.php

---
+++ crontabInstall.php
@@ -0,0 +1,6 @@
+step
+1) sudo crontab -e 
+2) same copy and paste (* * * * *) and almost same but isntead html change your folder/filename(fieldsales)
+3) ctrl+x(exit)
+4) Y
+5) sudo service cron restartn or optional(sudo service cron reload)
