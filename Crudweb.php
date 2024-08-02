598465acdb75a1f1b6336040924ff19b29c8b2ca
Omshiva Rana <161568312+omshivarana@users.noreply.github.com>
2024-08-02 16:35:07 +0530

    Create Crudweb.php
    crud web.php

---
+++ Crudweb.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\Staff\StaffController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard.index');
+})->middleware(['auth', 'verified'])->name('admin.dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+
+
+// code written by Omshiva Rana
+Route::prefix('staff')->group(function(){
+    Route::get('/index', [StaffController::class, 'index'])->name('admin.staff.index');
+    Route::post('/store', [StaffController::class, 'store'])->name('admin.staff.store');
+    Route::post('/update', [StaffController::class, 'update'])->name('admin.staff.update');
+    Route::post('/destroy/{id}', [StaffController::class, 'destroy'])->name('admin.staff.destroy');
+});
+// code written by Omshiva Rana
